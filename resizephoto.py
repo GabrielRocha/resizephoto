f2ad7906c5e1736a48db6354f23bb901513267ec
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2017-06-09 21:02:34 -0300

    Test and save image from json

---
+++ resizephoto.py
@@ -0,0 +1,50 @@
+from urllib.request import urlopen
+from io import BytesIO
+from PIL import Image as ImagePL
+from copy import copy
+from settings import SIZES, URL
+from models import Image
+import aiohttp
+import tempfile
+import json
+import asyncio
+
+
+def read_json(url):
+    try:
+        return json.loads(urlopen(url).read().decode("utf-8"))
+    except:
+        raise ValueError("Invalid Json")
+
+
+def read_images(url):
+    loop = asyncio.get_event_loop()
+    json_images = read_json(url)
+    tasks = [asyncio.ensure_future(generate_thumbnail(image['url']))
+             for image in json_images.get('images', [])]
+    wait = asyncio.wait(tasks)
+    result, _ = loop.run_until_complete(wait)
+    loop.close()
+
+
+def resize(raw_image):
+    image = ImagePL.open(BytesIO(raw_image))
+    images = {}
+    for size, dimension in SIZES:
+        handle, filepath = tempfile.mkstemp()
+        thumbnail_image = copy(image)
+        thumbnail_image.thumbnail(dimension)
+        thumbnail_image.save(filepath, "png")
+        images[size] = open(filepath, "rb")
+    return images
+
+
+async def generate_thumbnail(url):
+    async with aiohttp.request('GET', url) as response:
+        raw_image = await response.read()
+    images = resize(raw_image)
+    Image(url=url, **images).save()
+
+
+if __name__ == "__main__":
+    read_images(URL)
---
+++ tests/test_resizephoto.py
@@ -0,0 +1,68 @@
+from resizephoto import read_json, resize, generate_thumbnail, read_images
+from settings import URL, DATABASE_TEST as DATABASE
+from io import BufferedReader, BytesIO
+from PIL import Image as ImagePL
+from models import Image
+from pymodm import connect
+import pytest
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+connect('mongodb://{}:{}/{}'.format(DATABASE['host'],
+                                    DATABASE['port'],
+                                    DATABASE['name']))
+
+@pytest.fixture
+def clear():
+    Image.objects.delete()
+
+def test_json_contains_key_images():
+    assert "images" in read_json(URL)
+
+
+def test_json_contains_urls():
+    for itens in read_json(URL)['images']:
+        assert "url" in itens
+
+
+def test_invalid_json():
+    with pytest.raises(ValueError) as error:
+        read_json("http://error/error.json")
+    assert "Invalid Json" == str(error.value)
+
+
+@pytest.mark.parametrize("size", [("small", (320, 240)),
+                                  ("medium", (384, 288)),
+                                  ("large", (640, 480))])
+def test_resize(size):
+    with open(BASE_DIR+"/images/b737_5.jpg", "rb") as image:
+        images = resize(image.read())
+    tag, dimension = size
+    width, height = dimension
+    image = ImagePL.open(BytesIO(images[tag].read()))
+    assert tag in images
+    assert isinstance(images[tag], BufferedReader)
+    assert image.width == width
+    assert image.height == height
+
+
+@pytest.mark.asyncio
+async def test_generate(clear):
+    await generate_thumbnail("http://54.152.221.29/images/b737_5.jpg")
+    assert Image.objects.count() == 1
+    assert Image.objects.first().url == "http://54.152.221.29/images/b737_5.jpg"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("size", ["small", "medium", "large"])
+async def test_image_in_db(size, clear):
+    await generate_thumbnail("http://54.152.221.29/images/b737_5.jpg")
+    assert Image.objects.count() == 1
+    image = ImagePL.open(BASE_DIR+"/images/{}.jpg".format(size))
+    assert getattr(Image.objects.first(), size).image.tobytes() == image.tobytes()
+
+
+def test_read_images(clear):
+    read_images(URL)
+    assert Image.objects.count() == 10
